ecd40273fa38a407bca86f21f80675f2ca102e21
Kamal <k.ayadi@student.helmo.be>
2022-11-21 09:02:16 +0100

    test navbar

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,10 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom"
+import LoginPage from "../components/pages/LoginPage";
+import NavBar from "../components/organisms/NavBar";
 
 export default function Router(){
     return (
         <HashRouter>
+            <NavBar/>
             <Routes>
-                <Route path="/" element={<LoginPage/>}/>
+                <Route path="/LoginPage" element={< LoginPage />}/>
             </Routes>
         </HashRouter>
     );
--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -1,8 +1,148 @@
-import { AppBar } from "@mui/material";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import Gugi from "../../fonts/Gugi/Gugi-Regular.ttf";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {AlignHorizontalCenter} from "@mui/icons-material";
 
-  export default function NavBar(){
+const pages = ['Products', 'Pricing', 'Blog'];
+
+function NavBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const theme = createTheme({
+        typography: {
+            fontFamily: 'Gugi',
+        },
+        components: {
+            MuiCssBaseline: {
+                styleOverrides: `
+                @font-face {
+                  font-family: 'Gugi';
+                  font-style: normal;
+                  font-display: swap;
+                  font-weight: 400;
+                  src: local('Gugi'), local('Gugi-Regular'), url(${Gugi}) format('ttf');
+                  unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+                }
+              `,
+            },
+        },
+    });
 
     return (
-        <AppBar></AppBar>
-    )
-  }
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <ThemeProvider theme={theme}>
+                        <CssBaseline />
+                        <Box sx={{fontFamily: 'Gugi',}}>
+                            Student Lounge
+                        </Box>
+                    </ThemeProvider>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AlignHorizontalCenter>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href=""
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            minHeight="100vh"
+                        >
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                    </AlignHorizontalCenter>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default NavBar;
