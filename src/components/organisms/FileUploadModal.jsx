da4c4648683e917aef9a84e76ae965d823c66d30
E190449 <r.hendrice@student.helmo.com>
2022-12-07 17:25:48 +0100

    Fenêtre modale + upload fichier ok, à tester server error

---
+++ src/components/organisms/FileUploadModal.jsx
@@ -0,0 +1,60 @@
+import SendFormButton from "../atoms/SendFormButton";
+import CenteredModal from "../atoms/CenteredModal";
+import Title from "../atoms/Title";
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextField } from "@mui/material";
+import { FileOpen, TextFields } from "@mui/icons-material";
+
+export default function FileUploadModal({open, onClose, callback, repository}){
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event);
+        let form = event.target;
+        let file = form.file;
+        let type = form.type.value;
+        
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('type', type);
+
+        repository.uploadFile({formData: formData})
+            .then(
+                ok =>{
+                    callback();
+                    onClose();
+                }
+            )
+            .catch( 
+                error => {
+                    console.log(error);
+            });
+    }
+
+    return(
+        <CenteredModal open={open} onClose={onClose}>
+            <Title text={"Importer un fichier"}/>
+            <form onSubmit={handleSubmit}>
+            <FormControl>
+                <FormLabel>Fichier</FormLabel>
+                <TextField name={'file'} type={'file'} accept={'application/pdf'}/>
+            </FormControl>
+            <FormControl>
+                <FormLabel>Type</FormLabel>
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="type"
+                >
+                    <FormControlLabel value="0" control={<Radio />} label="Synthèse" >
+                        <FileOpen />
+                    </FormControlLabel>
+                    <FormControlLabel value="1" control={<Radio />} label="Notes" >
+                        <TextFields />
+                    </FormControlLabel>
+                </RadioGroup>
+            </FormControl>
+                <SendFormButton text={'Envoyer'}/>
+            </form>
+        </CenteredModal>
+    );
+}
